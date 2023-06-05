fc493a520a19430b1802c7838302b27ca2063dee
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2023-06-05 17:54:45 +0900

    [Silver IV] Title: 로프, Time: 424 ms, Memory: 27280 KB -BaekjoonHub

--- 백준/Silver/2217. 로프/로프.java
+++ 백준/Silver/2217. 로프/로프.java
@@ -9,19 +9,25 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
+        // 로프를 저장할 list
         ArrayList<Integer> list = new ArrayList<>();
-
+        //로프를 저장
         for(int i =0; i <n; i++){
             int tmp = Integer.parseInt(br.readLine());
             list.add(tmp);
         }
-
+        // 내림차순으로 로프를 정렬
         Collections.sort(list, Collections.reverseOrder());
-
+        // 최댓값을 설정
         long max = 0;
+        // 로프의 개수만큼 for문을 실행하며,
         for(int i =1; i<=list.size(); i++){
+            // 로프로 들 수 있는 최대 무게
             long num = (long) list.get(i - 1) * i;
-            if(num > max)
+            // 최대 무개가 max보다 작다면 continue
+            if(num < max) continue;
+            // 아니라면 max 값을 수정
+             else if (num > max)
                 max = num;
         }
         System.out.print(max);
