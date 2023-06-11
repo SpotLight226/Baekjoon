39ba9f202cf1f4cf00c9536d48c9b1998f9113fc
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2023-06-11 19:51:09 +0900

    [Bronze II] Title: 수 정렬하기, Time: 184 ms, Memory: 14904 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int  n = Integer.parseInt(br.readLine());
+
+        List<Integer> list = new ArrayList<>();
+
+        for(int i = 0; i<n; i++){
+            list.add(Integer.parseInt(br.readLine()));
+        }
+
+        Collections.sort(list);
+
+        for(int i = 0; i<list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+    }
+}
