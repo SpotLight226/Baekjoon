31383e72625cfe52f83dfacfe793572a618d19cb
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2023-06-11 23:12:20 +0900

    [Bronze I] Title: 팰린드롬수, Time: 128 ms, Memory: 14008 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
@@ -5,17 +5,17 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = null;
 
         while(true){
             String num = br.readLine();
-
+            // 입력 0일 때 종료
             if(num.equals("0")) break;
-
-            String reverse = "";
-
-            for(int i = num.length() - 1; i >= 0; i--){
-                reverse += num.charAt(i);
-            }
+            // StringBuilder 사용
+            sb = new StringBuilder(num);
+            // 입력을 역순으로
+            String reverse = sb.reverse().toString();
+            // 입력 == 역순 일 때, yes 아니면 no
 
             System.out.println((num.equals(reverse))? "yes" : "no");
         }
