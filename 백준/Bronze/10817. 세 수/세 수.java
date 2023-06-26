a7fa0857fd5fc8b95c8c6c2a06e34cb83b706b44
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2023-06-26 13:34:13 +0900

    [Bronze III] Title: 세 수, Time: 124 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Bronze/10817. 세 수/세 수.java
@@ -0,0 +1,27 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for(int i = 0; i < 3; i++){
+            list.add(Integer.parseInt(st.nextToken()));
+        }
+
+        Collections.sort(list);
+
+        System.out.println(list.get(1));
+    }
+
+}
+
