3068a28c7df8c21a9ea323bb10069aa6a018f31d
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2023-07-05 13:53:44 +0900

    [level 1] Title: 평균 구하기, Time: 2.48 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.java
+++ 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.java
@@ -1,13 +1,8 @@
+import java.util.*;
+
 class Solution {
     public double solution(int[] arr) {
-        double answer = 0;
-        double sum = 0;
-        for(int i : arr){
-            sum += i;
-        }
-        
-        answer = sum/arr.length;
-        
-        return answer;
+        // 배열 arr을 스트림으로 만든 후, 평균을 구하고, double로 반환
+        return Arrays.stream(arr).average().getAsDouble();
     }
 }
