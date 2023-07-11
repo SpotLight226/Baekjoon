0cd52ef5b23c609cdc092e7fe566509288b39efa
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2023-07-12 01:47:09 +0900

    [Silver V] Title: 단어 정렬, Time: 364 ms, Memory: 26140 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,54 +1,34 @@
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Comparator;
+import java.io.*;
+import java.util.*;
 
-public class Main {
-
-
+class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
 
-        String[] arr = new String[n];
-
-        for(int i = 0; i < n; i++){
-            arr[i] = br.readLine();
+        ArrayList<String> list = new ArrayList<>();
+        while(n --> 0){
+            list.add(br.readLine());
         }
 
-        Arrays.sort(arr, new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2) {
-                // 단어 길이가 같을 때
-                if(o1.length() == o2.length()){
-                    return o1.compareTo(o2);
-                }
-                // 그 외
-                  else {
-                    return o1.length() - o2.length();
-                }
+        list.sort((o1,o2) -> {
+            if(o1.length() == o2.length()){
+                return o1.compareTo(o2);
             }
+            return o1.length() - o2.length();
         });
 
-        StringBuilder sb = new StringBuilder();
-
-        // 첫 단어 sb에 입력
-        sb.append(arr[0]).append("\n");
-
-        // 앞의 단어와 뒤의 단어가 같다면 입력 X.
-        for(int i = 1; i < n; i++){
-            // 앞의 단어와 다를 때만 입력한다
-            if(!arr[i].equals(arr[i-1])){
-                sb.append(arr[i]).append("\n");
+        sb.append(list.get(0)).append("\n");
+        for(int i = 1; i < list.size(); i++){
+            if(!list.get(i).equals(list.get(i-1))){
+                sb.append(list.get(i)).append("\n");
             }
         }
-
         System.out.println(sb);
-
     }
-
 }
+
+
 
