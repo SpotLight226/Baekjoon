c3b3ba1593010bd024e4e335c8698ce4bc76e00c
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2023-08-02 06:02:48 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.19 ms, Memory: 84.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -4,7 +4,17 @@ class Solution {
         List<String> list = Arrays.asList(my_string.split(""));
 
         Collections.reverse(list);
-        
+
         return String.join("", list);
+        
+        /*
+        
+        StringBuilder sb = new StringBuilder();
+        sb.append(my_String);
+        sb.reverse();
+        
+        return sb.toString();
+        
+        */
     }
 }
