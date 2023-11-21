f6911a0dc7e809556e95b576aed41c2d165de77f
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2023-11-21 23:54:13 +0900

    [Bronze II] Title: 손익분기점, Time: 128 ms, Memory: 14152 KB -BaekjoonHub

---
+++ 백준/Bronze/1712. 손익분기점/손익분기점.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+ 
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+		StringTokenizer st = new StringTokenizer(br.readLine());
+ 
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+        
+        // ( n x 상품가격 ) = 불변 비용 + ( n x 가변 비용 )
+        // n == A / C-B
+        // 이익은 n 이 양수 즉 + 1
+		if (C <= B) {
+			System.out.println("-1");
+		} 
+		else {
+			System.out.println((A/(C-B))+1);
+		}
+	}
+}
+ 
+ 
