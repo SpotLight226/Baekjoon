1225c92482718e4077187e180c7d9082f729e2fa
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2023-12-11 23:56:02 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 788 ms, Memory: 54000 KB -BaekjoonHub

---
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+class Main{
+    public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		Map<String, Integer> checkList = new HashMap<>();
+		for(int i=0; i<n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			String name = st.nextToken();
+			String status= st.nextToken();
+			if(checkList.containsKey(name)) {
+				checkList.remove(name);
+			}else {
+				if(status.equals("enter")){
+					checkList.put(name, 1);
+				}
+			}
+		}
+
+		StringBuilder sb= new StringBuilder();
+		List<String> curList = new ArrayList<>(checkList.keySet());
+		Collections.sort(curList,(a,b) -> b.compareTo(a));
+		for(String s : curList) {
+			sb.append(s+"\n");
+		}
+		System.out.println(sb.toString());
+		
+	}
+}
+
