72e3a5c910eead075cf74c7c54044f0fbd401d79
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2024-01-05 23:59:14 +0900

    [Bronze III] Title: 사과, Time: 128 ms, Memory: 14288 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/10833. 사과/사과.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		int n = Integer.parseInt(br.readLine());
+		int result = 0;
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			result += b % a;
+		}
+		br.close();
+		System.out.println(result);
+	}
+
+}
+
