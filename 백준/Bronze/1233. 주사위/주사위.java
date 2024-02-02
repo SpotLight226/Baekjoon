5b3d129268ab673522b9113ba654dd74651958b7
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2024-02-03 00:30:17 +0900

    [Bronze II] Title: 주사위, Time: 128 ms, Memory: 14224 KB -BaekjoonHub

---
+++ 백준/Bronze/1233. 주사위/주사위.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int s1 = Integer.parseInt(st.nextToken());
+        int s2 = Integer.parseInt(st.nextToken());
+        int s3 = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[s1 + s2 + s3 - 2];
+
+        for (int i = 1; i <= s1; i++) {
+            for (int j = 1; j <= s2; j++) {
+                for (int k = 1; k <= s3; k++) {
+                    int sum = i + j + k;
+                    arr[sum - 3]++;
+                }
+            }
+        }
+
+        int max = 0;
+        int answer = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (max < arr[i]) {
+                max = arr[i];
+                answer = i + 3;
+            }
+        }
+
+        bw.write(String.valueOf(answer));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
