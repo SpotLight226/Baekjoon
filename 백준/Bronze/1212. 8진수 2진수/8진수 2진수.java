ea2ca6b5598a948e2d17c427ac54b07cb532d308
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2024-02-03 00:15:52 +0900

    [Bronze II] Title: 8진수 2진수, Time: 468 ms, Memory: 50736 KB -BaekjoonHub

--- 백준/Bronze/1212. 8진수 2진수/8진수 2진수.java
+++ 백준/Bronze/1212. 8진수 2진수/8진수 2진수.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.math.BigInteger;
+import java.text.BreakIterator;
 import java.util.*;
 
 public class Main {
@@ -9,13 +10,34 @@ public class Main {
 
         String octalNum = br.readLine();
 
-        BigInteger decimalNum = new BigInteger(octalNum, 8);
-
-        String binaryString = decimalNum.toString(2);
+        String binaryString = octalToBinary(octalNum);
 
         bw.write(binaryString);
         bw.flush();
         bw.close();
         br.close();
+    }
+
+    static String octalToBinary(String octalNum) {
+        StringBuilder binary = new StringBuilder();
+
+        // 첫 번째 숫자의 이진수 변환 후 앞의 0 제거
+        int firstDigit = Character.getNumericValue(octalNum.charAt(0));
+        String binaryDigit = Integer.toBinaryString(firstDigit);
+        binary.append(binaryDigit);
+
+        for (int i = 1; i < octalNum.length(); i++) {
+            int decimalValue = Character.getNumericValue(octalNum.charAt(i));
+            binaryDigit = Integer.toBinaryString(decimalValue);
+
+            // 3비트로 만들기 위해 앞에 0 추가
+            while (binaryDigit.length() < 3) {
+                binaryDigit = "0" + binaryDigit;
+            }
+
+            binary.append(binaryDigit);
+        }
+
+        return binary.toString();
     }
 }
