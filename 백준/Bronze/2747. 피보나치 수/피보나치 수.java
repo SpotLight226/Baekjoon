18673b342d1e42742e8116c8cac87c9ea73ec5da
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2024-04-08 22:35:15 +0900

    [Bronze II] Title: 피보나치 수, Time: 120 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Bronze/2747. 피보나치 수/피보나치 수.java
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -2,26 +2,40 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
+    static int[] dp;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         int n = Integer.parseInt(br.readLine());
-
-        int[] dp = new int[46];
-
-        dp[1] = 1;
-
-        for (int i = 2; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
-        }
-
-        bw.write(String.valueOf(dp[n]));
+    
+        bw.write(String.valueOf(calcFibonacci(n)));
         bw.flush();
         bw.close();
         br.close();
-
     }
+        
+    /*
+    재귀는 시간 복잡도가 높아 시간초과
+    static int calcFibonacci(int n) {
 
+        if (n <= 1) {
+            return n;
+        } else {
+            return calcFibonacci(n - 1) + calcFibonacci(n - 2);
+        }
+    }
+    */
+    
+    static int calcFibonacci(int n) {
+
+        if(n <= 1) return n;
+
+        dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
 }
