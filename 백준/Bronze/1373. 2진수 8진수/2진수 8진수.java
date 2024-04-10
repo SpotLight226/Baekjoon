dd286cfe0b44a3ef3661bc9baf26eef0de286c69
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2024-04-10 21:45:56 +0900

    [Bronze I] Title: 2진수 8진수, Time: 2120 ms, Memory: 91664 KB -BaekjoonHub

--- 백준/Bronze/1373. 2진수 8진수/2진수 8진수.java
+++ 백준/Bronze/1373. 2진수 8진수/2진수 8진수.java
@@ -7,8 +7,11 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        // 주어지는 수의 길이는 1,000,000을 넘지 않는다. >> int 형의 범위를 넘어간다
+        // BigInteger 개체를 생성할 떄, 2 를 넣으면 10진수로 변환
         BigInteger decimalN = new BigInteger(br.readLine(), 2);
 
+        // bigInteger 10진수 형태를 8진수로 변환
         String answer = decimalN.toString(8);
 
         bw.write(answer);
