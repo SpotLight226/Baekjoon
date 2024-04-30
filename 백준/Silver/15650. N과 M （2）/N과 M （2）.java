64109e82ce5d93f81483daec09fb036bef8e6bad
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2024-04-30 22:07:12 +0900

    [Silver III] Title: N과 M (2), Time: 124 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -3,7 +3,6 @@ import java.util.*;
 
 public class Main {
     static int n, m;
-    static boolean[] check;
     static int[] arr;
     static StringBuilder sb = new StringBuilder();
 
@@ -16,41 +15,31 @@ public class Main {
         m = Integer.parseInt(st.nextToken()); // 자연수의 개수
 
         arr = new int[m]; // 자연수를 저장할 배열
-        check = new boolean[n]; // n 까지 자연수 중복을 체크하기 위한 배열
 
-        dfs(0);
+        // 오름차순인지를 파악하는 함수를 작성하는 것이 아닌, 현재 위치를 표시하는 인자를 넘겨주어 오름차순으로만 진행하게
+        // 만드는 풀이 방식
+        // 이게 더 쉬운 듯, 중복 확인하는 boolean 배열이 필요 없다
+        dfs(1, 0);
         bw.write(sb.toString());
         bw.flush();
         bw.close();
         br.close();
     }
 
-    static void dfs(int depth) {
+    // now => 현재 위치 -> 어디서 부터 시작하는지를 의미하는 변수
+    static void dfs(int now, int depth) {
+        // 어차피 depth 가 m 에 도달하지 않으면, sb 에 수열을 저장할 필요도 없다
         if (depth == m) {
-            if (checkAsc(arr)) {
-                for (int i : arr) {
-                    sb.append(i).append(' ');
-                }
-                sb.append("\n");
+            for (int i : arr) {
+                sb.append(i).append(' ');
             }
+            sb.append("\n");
             return;
         }
-
-        for (int i = 0; i < n; i++) {
-            if (!check[i]) {
-                arr[depth] = i + 1;
-                check[i] = true;
-                dfs(depth + 1);
-                check[i] = false;
-            }
+        // 현재 i 값을 arr 에 저장하므로, 다음 재귀 호출에는 +1 한 값을 넘겨주어 호출한다
+        for (int i = now; i <= n; i++) {
+            arr[depth] = i;
+            dfs(i + 1, depth + 1);
         }
-    }
-
-    static boolean checkAsc(int[] arr) {
-
-        for (int i = 1; i < arr.length; i++) {
-            if(arr[i -1] > arr[i]) return false;
-        }
-        return true;
     }
 }
