79865e2b8ce8b19883ea5a8f972d39382014fd95
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2024-07-23 21:26:59 +0900

    [level 1] Title: K번째수, Time: 0.54 ms, Memory: 83.3 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -3,6 +3,7 @@ class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
         
+        /*
         for (int i = 0; i < commands.length; i++) {
 
             int[] com = new int[3]; // 각 command
@@ -19,7 +20,18 @@ class Solution {
 
             answer[i] = copyArr[com[2]]; // 정답 배열에 정렬된 copyArr의 해당 인덱스 숫자를 넣는다
         }
+        */
         
+        // 위의 이중 for문을 쓸 필요 없이
+        // 위의 이중 for문을 쓸 필요 없이
+        for(int i = 0; i < commands.length; i++) {
+            // commands는 이중 배열로 내부의 각 배열의 첫 요소는 시작점, 두 번째 요소는 끝 점이다
+            // 위에선 각 commands의 각 줄을 뽑을 때 다 -1 했지만 copyOfRange의 특성을 사용해 시작점만 -1
+            int[] copyArr = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
+            Arrays.sort(copyArr);
+            // commands의 각 줄에서 가장 끝 요소가 선택 인덱스
+            answer[i] = copyArr[commands[i][2]-1];
+        }
         
         return answer;
     }
