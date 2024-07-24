160ce0ad7d7cc8d867b9856d35d8e804c2a85c4d
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2024-07-24 22:34:52 +0900

    [level 2] Title: 가장 큰 수, Time: 169.32 ms, Memory: 121 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,5 +1,5 @@
 import java.util.*;
-
+/*
 class Solution {
     public String solution(int[] numbers) {
        StringBuilder sb = new StringBuilder();
@@ -16,8 +16,8 @@ class Solution {
                     if (o1.length() == o2.length()) { // 길이가 같을 때
                         return o2.compareTo(o1);
                     }
-
-                    if (o1.length() < o2.length()) {
+                    // 각 숫자의 길이가 다르다면 짧은 숫자의 길이 기준으로 한 자리씩 비교
+                    if (o1.length() < o2.length()) { 
                         for (int i = 0; i < o1.length(); i++) {
                             if (o1.charAt(i) != o2.charAt(i)) {
                                 return o2.substring(i, i + 1).compareTo(o1.substring(i, i + 1));
@@ -43,7 +43,38 @@ class Solution {
         for (String str : arr) {
             sb.append(str);
         }
-
+        // 가장 앞 글자가 0이라면 0을 리턴 아니면 sb를 String으로 리턴
         return sb.substring(0, 1).equals("0") ? "0" : String.valueOf(sb);
+    }
+}
+*/
+
+class Solution {
+    public String solution(int[] numbers) {
+        List<String> list = new ArrayList<>();
+
+        for (int number : numbers) {
+            list.add(String.valueOf(number));
+        }
+
+        Collections.sort(list, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                String order1 = o1 + o2;
+                String order2 = o2 + o1;
+                return order2.compareTo(order1);
+            }
+        });
+
+        if (list.get(0).equals("0")) {
+            return "0";
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (String num : list) {
+            sb.append(num);
+        }
+
+        return sb.toString();
     }
 }
