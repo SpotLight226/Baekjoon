16bcf95ba359319d0470eb7f42ff7f8ccc908816
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2024-08-05 20:51:05 +0900

    [level 0] Title: 숫자 찾기, Time: 0.06 ms, Memory: 85.9 MB -BaekjoonHub

--- 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
@@ -1,5 +1,6 @@
 class Solution {
     public int solution(int num, int k) {
+        /*
         int answer = -1;
         
         String[] arr = String.valueOf(num).split("");
@@ -12,5 +13,14 @@ class Solution {
         }
         
         return answer;
+        */
+        
+        String numStr = String.valueOf(num);
+        String kStr = String.valueOf(k);
+        
+        int answer = numStr.indexOf(kStr);
+        
+        // answer 이 0보다 작다는 것은 없다는 것 그래서 - 1 반환 있다면 index + 1
+        return answer < 0 ? -1 : answer + 1;
     }
 }
