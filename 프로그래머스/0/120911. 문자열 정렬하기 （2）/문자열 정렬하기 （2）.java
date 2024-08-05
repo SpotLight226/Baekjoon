f82f72f465ccdd9f2f4a74a3e8c7746f26b5518a
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2024-08-05 20:56:52 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.52 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -1,6 +1,8 @@
 import java.util.*;
 class Solution {
     public String solution(String my_string) {
+        /*
+        // sb 이용해서 split한 배열을 정렬해 담아 리턴
         StringBuilder sb = new StringBuilder();
         
         String[] arr = my_string.toLowerCase().split("");
@@ -12,5 +14,11 @@ class Solution {
         }
         
         return sb.toString();
+        */
+        
+        char[] arr = my_string.toLowerCase().toCharArray(); // 새로운 String 객체로 만들기 위해 char배열로
+        Arrays.sort(arr); // 정렬
+        
+        return new String(arr);
     }
 }
