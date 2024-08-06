2265bcc114121bfe4e8c9baa1730de98f17dd700
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2024-08-06 21:30:21 +0900

    [level 0] Title: A로 B 만들기, Time: 1.62 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.java
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.java
@@ -1,29 +1,29 @@
 import java.util.*;
 class Solution {
     public int solution(String before, String after) {
-        HashMap<Character, Integer> beforeHash = new HashMap<>();
-        HashMap<Character, Integer> afterHash = new HashMap<>();
+        HashMap<Character, Integer> beforeHash = new HashMap<>(); // before의 해쉬 맵
+        HashMap<Character, Integer> afterHash = new HashMap<>(); // after의 해쉬 맵
 
         for (int i = 0; i < before.length(); i++) {
             char beforeC = before.charAt(i);
             char afterC = after.charAt(i);
-
+            // 각각의 해쉬 맵에 값이 없다면 0 + 1, 값이 있다면 그 값에 + 1 해서 저장
             beforeHash.put(beforeC, beforeHash.getOrDefault(beforeC, 0) + 1);
             afterHash.put(afterC, afterHash.getOrDefault(afterC, 0) + 1);
         }
 
-        boolean check = true;
-
+        boolean check = true; // 가능한지 확인할 변수 
+        // keySet으로 반복문
         for (char key : beforeHash.keySet()) {
-            int beforeValue = beforeHash.get(key);
-            int afterValue = afterHash.getOrDefault(key, 0);
-
+            int beforeValue = beforeHash.get(key); // before 의 값
+            int afterValue = afterHash.getOrDefault(key, 0); // after의 값 (없다면 0)
+            // before 과 after가 다르면 check를 false로 만들고 반복 종료
             if (beforeValue != afterValue) {
                 check = false;
                 break;
             }
         }
-        
+        // check 가 true 면 1, false 면 0 반환
         return check ? 1 : 0;
     }
 }
