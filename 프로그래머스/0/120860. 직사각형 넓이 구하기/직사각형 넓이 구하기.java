d459f4958e497ce1d538711662b46b9bf39e9bb4
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2024-08-07 21:43:48 +0900

    [level 0] Title: 직사각형 넓이 구하기, Time: 0.02 ms, Memory: 77.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int solution(int[][] dots) {
+        int minX = Integer.MAX_VALUE; // 가장 작은 x좌표
+        int maxX = Integer.MIN_VALUE; // 가장 큰 x좌표
+        int minY = Integer.MAX_VALUE; // 가장 작은 y좌표
+        int maxY = Integer.MIN_VALUE; // 가장 큰 y좌표
+
+        for (int[] dot : dots) {
+            int x = dot[0]; // x 좌표
+            int y = dot[1]; // y 좌표
+            
+            // 각 조건에 따라 최대, 최소 X,Y 좌표를 구함
+            if(x < minX) minX = x;
+            if(x > maxX) maxX = x;
+            if(y < minY) minY = y;
+            if(y > maxY) maxY = y;
+        }
+ 
+        int width = maxX - minX; // 가로
+        int height = maxY - minY; // 세로
+        
+        return width * height; // 넓이
+    }
+}
