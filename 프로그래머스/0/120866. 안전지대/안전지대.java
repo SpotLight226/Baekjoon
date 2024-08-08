176fdc3b21c9680636c7a92741cce9af8617e72f
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2024-08-08 22:30:36 +0900

    [level 0] Title: 안전지대, Time: 0.04 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/0/120866. 안전지대/안전지대.java
+++ 프로그래머스/0/120866. 안전지대/안전지대.java
@@ -1,6 +1,7 @@
 import java.util.*;
 class Solution {
     public int solution(int[][] board) {
+        /*
         // set 을 사용해서 중복을 제거한 위험 지역 표시 및 계산
         int len = board.length;
 
@@ -34,6 +35,43 @@ class Solution {
         // 전체 칸 수에서 위험 지역 칸 수를 빼서 안전한 지역의 칸 수 계산
         int totalCells = len * len;
         int answer = totalCells - dangerZone.size();
+
+        return answer;
+        */
+        // 위험지역 체크를 통한 정석
+        int len = board.length;
+
+        // 위, 아래, 좌, 우, 좌 대각선 위 , 좌 대각선 아래, 우 대각선 위, 우 대각선 아래,
+        int[] dangerX = {1, -1, 0, 0, 1, -1, 1, -1};
+        int[] dangerY = {0, 0, 1, -1, 1, 1, -1, -1};
+
+        boolean[][] dangerZone = new boolean[len][len]; // 위험지역 체크
+
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < len; j++) {
+                if (board[i][j] == 1) {
+
+                    dangerZone[i][j] = true; // 위험지역
+
+                    for (int z = 0; z < 8; z++) {
+                        int xRange = i + dangerX[z];
+                        int yRange = j + dangerY[z];
+                        // 범위 확인후 위험지역으로
+                        if (xRange >= 0 && xRange < len && yRange >= 0 && yRange < len) {
+                            dangerZone[xRange][yRange] = true;
+                        }
+                    }
+                }
+            }
+        }
+
+        int answer = 0;
+
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < len; j++) {
+                if(!dangerZone[i][j]) answer++; // 위험지역이 아니라면 answer + 1
+            }
+        }
 
         return answer;
     }
