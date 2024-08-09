24c4d35b9c589c7d5762fae9c4c3c5e1247566c3
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2024-08-09 21:22:28 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.29 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.java
+++ 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.java
@@ -1,6 +1,8 @@
 class Solution {
     public int solution(int n) {
-        int[] arr = new int[101];  // 100 크기의 배열을 선언합니다.
+        /*
+        // 미리 3x 마을의 숫자를 계산해둔 arr 배열을 만들어 푸는 방식
+        int[] arr = new int[101];  // 101 크기의 배열을 선언합니다.
         int x = 1;  // 3x 마을의 숫자를 시작합니다.
         int index = 1;  // 배열의 인덱스를 1부터 시작합니다.
 
@@ -16,5 +18,19 @@ class Solution {
         }
         
         return arr[n];
+        */
+        // for문으로 answer의 값을 계산하여 3의 배수 또는 answer에 3이 포함되어 있으면 i 를 -1해서
+        // 반복문이 더 돌아갈 수 있게 한다 
+        // 이 방법 좋은듯
+        int answer = 0;
+
+        for (int i = 1; i <= n; i++) {
+            answer++;
+            if (answer % 3 == 0 || String.valueOf(answer).contains("3")) {
+                i--;
+            }
+        }
+
+        return answer;
     }
 }
