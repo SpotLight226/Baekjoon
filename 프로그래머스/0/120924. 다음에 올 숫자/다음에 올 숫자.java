e9a3e7fdc3d378d9d66c95eb5955e3fc28c65a7e
SpotLight226 <127032252+SpotLight226@users.noreply.github.com>
2024-08-13 21:42:57 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.03 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.java
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -1,10 +1,11 @@
 class Solution {
     public int solution(int[] common) {
          int len = common.length;
-
-        if (common[1] - common[0] == common[2] - common[1]) {
+        // 등비, 등차를 판단하기 위해서 
+        // 0, 1, 2 항의 값의 차를 확인한다
+        if (common[1] - common[0] == common[2] - common[1]) { // 등차
             return common[len - 1] + common[1] - common[0];
-        } else {
+        } else { // 등비
             return common[len - 1] * (common[1] / common[0]);
         }
     }
